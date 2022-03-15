ab6377dc4f870396c4e4735d41a9bddad3470973
Arthur Gregório <arthurgregorioleal@gmail.com>
2022-03-15 15:56:37 -0300

    Feat: Script inicial para extração de métricas DIT, LOC, CBO e LCOM

--- processor/script.py
+++ processor/script.py
@@ -1,9 +1,28 @@
-import sys
-
+import sys, os, json
 
 def main():
-    print ("Hello, from script, received: ")
-    for arg in sys.argv:
-        print(arg)
+    repo_url: str = sys.argv[1]
+    os.system("git clone {} --filter=blob:none /git".format(repo_url))
+    os.system("java -jar /usr/bin/ck.jar /git false 0 False /tmp/")
+    os.system("csv-to-json /tmp/class.csv /tmp/class.json")
+    file = open('/tmp/class.json')
+    data = json.load(file)
+
+    attributes = {
+        'dit': float(data[0]['dit']),
+        'loc': 0.0,
+        'cbo': 0.0,
+        'lcom': 0.0
+    }
+    
+    for d in data:
+        attributes['loc'] += int(d['loc'])
+        attributes['cbo'] += int(d['cbo'])
+        attributes['lcom'] += int(d['lcom'])
+        
+    attributes['cbo'] = attributes['cbo']/len(data)
+    attributes['lcom'] = attributes['lcom']/len(data)
+    
+    print(attributes)
     
 main()
