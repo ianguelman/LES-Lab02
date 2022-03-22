01464d87de9829a9d6272d5ec22078b80eeeab5e
Arthur Gregório <arthurgregorioleal@gmail.com>
2022-03-22 17:54:39 -0300

    Feat: Implementada execução do script execute.py em multithread

--- index.py
+++ index.py
@@ -1,5 +1,27 @@
+from concurrent.futures import process
 from scripts.query import run as query
 from scripts.execute import run as execute
+from utils.mongo import Mongo
+from threading import Thread
 
 query()
-execute()
+
+threads = 24
+documents = Mongo().get_all_documents()
+
+def loop(start, stop):
+    for i in range(start, stop):
+        if not documents[i]['processed']:
+            execute(documents[i]['url'])
+            
+size = len(documents) / threads
+
+for i in range(threads):
+    args = (int(size * i), int(size * i + size -1)) if i != threads - 1 else (int(size * i), int(size * i + size))
+    t = Thread(target = loop, args = args)
+    t.start()
+    
+
+
+
+
--- scripts/execute.py
+++ scripts/execute.py
@@ -1,22 +1,21 @@
+from distutils.util import execute
 from utils.mongo import Mongo
 import os, json, subprocess, re
 
-def run():
-    # documents = Mongo().get_all_documents()
-    url = "https://github.com/spring-projects/spring-boot"
-    has_docker_image = os.popen('docker image ls | grep lab02-processor-script').read()
-    if not has_docker_image:
-        os.system('cd processor && docker build . -t lab02-processor-script')
-    proc = subprocess.Popen(
-        ['docker', 'run', 'lab02-processor-script', url], 
-        stdout=subprocess.PIPE, 
-        stderr=subprocess.STDOUT
-    )
-    output = re.search(r'({.*})', str(proc.communicate()[0])).group(1)
-    parsed_json = json.loads(output)
-    Mongo().update_one({'url': url}, {'$set' : parsed_json })
-    Mongo().update_one({'url': url}, {'$set' : { 'processed': True } })
-    print (f"Métricas salvas do repositório {url}")
-    # for doc in documents:
-        # result = os.popen('docker run lab02-processor-script {}'.format(doc['url'])).read()
-        # print(documents[0]['url'])
+def run(url):
+    try:
+        has_docker_image = os.popen('docker image ls | grep lab02-processor-script').read()
+        if not has_docker_image:
+            os.system('cd processor && docker build . -t lab02-processor-script')
+        proc = subprocess.Popen(
+            ['docker', 'run', 'lab02-processor-script', url], 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.STDOUT
+        )
+        output = re.search(r'({.*})', str(proc.communicate()[0])).group(1)
+        parsed_json = json.loads(output)
+        Mongo().update_one({'url': url}, {'$set' : parsed_json })
+        Mongo().update_one({'url': url}, {'$set' : { 'processed': True } })
+        print (f"Métricas salvas do repositório {url}")
+    except Exception as e:
+        print(f"Failed to extract metrics from {url}, error: {e}")
